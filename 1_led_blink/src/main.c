c2f68fcc334e068f79ae22d0722f4d1317b91e9b
Nikita_Tomilin <nikita.tomilin.vasil@mail.ru>
2025-05-11 21:06:51 +0500

    new_dir

---
+++ 1_led_blink/src/main.c
@@ -0,0 +1,28 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"           // vTaskDelay
+#include "driver/gpio.h"             // GPIO API
+
+
+
+#define LED_GPIO GPIO_NUM_2
+
+void app_main() {
+    // 1. Cбрасываем пин к дефолту
+    gpio_reset_pin(LED_GPIO);
+    
+    // 2. Конфигурируем направление — выход
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+    
+    // 3. Бесконечный цикл мигания
+    while (1) {
+        // Включаем светодиод (уровень HIGH)
+        gpio_set_level(LED_GPIO, 1);
+        // Задержка 500 мс
+        vTaskDelay(pdMS_TO_TICKS(500));
+        
+        // Выключаем светодиод (уровень LOW)
+        gpio_set_level(LED_GPIO, 0);
+        // Задержка 500 мс
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
